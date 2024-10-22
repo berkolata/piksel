e580ddc8e744de4d823f3df25a1c817e743e81f1
berkolata <moderneinteraktif@gmail.com>
2024-10-23 02:28:54 +0300

    v2 updates
    blog, contact, support sayfaları eklendi. teknolojiler sayfası güncellendi.
    tüm sayfalara active class geldi.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener("DOMContentLoaded", function () {
   // Header'ı yükle
   fetch("components/header.html")
@@ -15,79 +14,97 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("footer").innerHTML = data;
     })
     .catch((error) => console.error("Footer Yükleme Hatası:", error));
-});
 
-//menu-animasted
-document.querySelectorAll(".menu-item").forEach((item) => {
-  item.addEventListener("mouseenter", function () {
-    const line = item.querySelector(".underline");
-    gsap.to(line, { scaleX: 1, duration: 0.3 });
-  });
-
-  item.addEventListener("mouseleave", function () {
-    const line = item.querySelector(".underline");
-    gsap.to(line, { scaleX: 0, duration: 0.3 });
-  });
-});
-
-//text animation
-const wordsArray = [
-  "Merhaba", // Türkçe
-  "नमस्ते", // Hintçe
-  "ہیلو", // Urduca
-  "Hola", // İspanyolca
-  "Bonjour", // Fransızca
-  "Hallo", // Almanca
-  "Ciao", // İtalyanca
-  "Привет", // Rusça
-  "你好", // Çince
-  "مرحبا", // Arapça
-  "こんにちは", // Japonca
-  "안녕하세요", // Korece
-];
-
-function displayWords() {
-  let index = 0;
-  const intervalId = setInterval(() => {
-    if (index < wordsArray.length) {
-      document.getElementById("container").textContent = wordsArray[index];
-      index++;
+  // Aktif menüleri denetle
+  var currentPath = window.location.pathname;
+  document.querySelectorAll("nav a").forEach(function (link) {
+    if (link.getAttribute("href") === currentPath) {
+      link.classList.add("text-blue-600", "active");
     } else {
-      clearInterval(intervalId);
+      link.classList.remove("text-blue-600", "active");
     }
-  }, 300);
-}
+  });
 
-document.addEventListener("DOMContentLoaded", function () {
+  // Menu animation
+  document.querySelectorAll(".menu-item").forEach((item) => {
+    item.addEventListener("mouseenter", function () {
+      const line = item.querySelector(".underline");
+      gsap.to(line, { scaleX: 1, duration: 0.3 });
+    });
+
+    item.addEventListener("mouseleave", function () {
+      const line = item.querySelector(".underline");
+      gsap.to(line, { scaleX: 0, duration: 0.3 });
+    });
+  });
+
+  // Text animation
+  const wordsArray = [
+    "Merhaba", // Türkçe
+    "नमस्ते", // Hintçe
+    "ہیلو", // Urduca
+    "Hola", // İspanyolca
+    "Bonjour", // Fransızca
+    "Hallo", // Almanca
+    "Ciao", // İtalyanca
+    "Привет", // Rusça
+    "你好", // Çince
+    "مرحبا", // Arapça
+    "こんにちは", // Japonca
+    "안녕하세요", // Korece
+  ];
+
+  function displayWords() {
+    let index = 0;
+    const intervalId = setInterval(() => {
+      if (index < wordsArray.length) {
+        document.getElementById("container").textContent = wordsArray[index];
+        index++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 300);
+  }
+
+  displayWords(); // Kelime gösterimini başlat
+
   // Sayfa yüklendiğinde loading gizle
   setTimeout(() => {
     document.body.classList.remove("is-loading");
   }, 2000); // 2 saniye
-});
 
-displayWords();
+  // Lion animasyonu
+  const lion = document.getElementById("lion");
+  document.addEventListener("mousemove", (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
 
-//aslan-animation
-const lion = document.getElementById("lion");
+    // Ekran merkezini hesapla
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
 
-document.addEventListener("mousemove", (e) => {
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
+    // Hareket miktarı hesapla
+    const xOffset = (mouseX - centerX) * 0.1; // 0.1 çarpanı hareketi yavaşlatır
+    const yOffset = (mouseY - centerY) * 0.1;
 
-  // Ekran merkezini hesapla
-  const centerX = window.innerWidth / 2;
-  const centerY = window.innerHeight / 2;
+    gsap.to(lion, {
+      x: xOffset,
+      y: yOffset,
+      duration: 0.3,
+      ease: "power2.out",
+    });
+  });
 
-  // Hareket miktarı hesapla
-  const xOffset = (mouseX - centerX) * 0.1; // 0.1 çarpanı hareketi yavaşlatır
-  const yOffset = (mouseY - centerY) * 0.1;
+  // Menu show-hide animation
+  document.querySelectorAll(".menu-item").forEach((item) => {
+    item.addEventListener("mouseenter", function () {
+      const line = item.querySelector(".underline");
+      gsap.to(line, { scaleX: 1, duration: 0.3 });
+    });
 
-  gsap.to(lion, {
-    x: xOffset,
-    y: yOffset,
-    duration: 0.3,
-    ease: "power2.out",
+    item.addEventListener("mouseleave", function () {
+      const line = item.querySelector(".underline");
+      gsap.to(line, { scaleX: 0, duration: 0.3 });
+    });
   });
 });
-
-//menu-show-hide
